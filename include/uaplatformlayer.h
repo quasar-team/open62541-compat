f40febdd03724917d5579561549c2652af3a4718
Piotr Nikiel <piotr@nikiel.info>
2017-11-29 16:32:55 +0100

    added platformlayer compat

---
+++ include/uaplatformlayer.h
@@ -0,0 +1,18 @@
+/*
+ * uaplatformlayer.h
+ *
+ *  Created on: 29 Nov 2017
+ *      Author: pnikiel
+ */
+
+#ifndef OPEN62541_COMPAT_INCLUDE_UAPLATFORMLAYER_H_
+#define OPEN62541_COMPAT_INCLUDE_UAPLATFORMLAYER_H_
+
+
+namespace UaPlatformLayer
+{
+    void init() {}; // this obviously is a fake
+}
+
+
+#endif /* OPEN62541_COMPAT_INCLUDE_UAPLATFORMLAYER_H_ */

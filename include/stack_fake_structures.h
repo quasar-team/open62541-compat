990707575fe270204fdbb66a151a4a2e07f2208c
Piotr Nikiel <piotr@nikiel.info>
2017-12-04 12:16:43 +0100

    fixed some headers

--- include/stack_fake_structures.h
+++ include/stack_fake_structures.h
@@ -1,8 +1,22 @@
-/*
- * stack_fake_structures.h
+/* © Copyright Piotr Nikiel, CERN, 2017.  All rights not expressly granted are reserved.
+ *  stack_fake_structures.h
  *
- *  Created on: 27 Nov 2017
- *      Author: pnikiel
+ *  Created on: 27 Nov, 2017
+ *      Author: Piotr Nikiel <piotr@nikiel.info>
+ *
+ *  This file is part of Quasar.
+ *
+ *  Quasar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public Licence as published by
+ *  the Free Software Foundation, either version 3 of the Licence.
+ *
+ *  Quasar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public Licence for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Quasar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef OPEN62541_COMPAT_INCLUDE_UACLIENT_STACK_FAKE_STRUCTURES_H_
--- include/uaclient/uasession.h
+++ include/uaclient/uasession.h
@@ -1,8 +1,22 @@
-/*
- * uasession.h
+/* © Copyright Piotr Nikiel, CERN, 2017.  All rights not expressly granted are reserved.
+ *  uasession.h
  *
- *  Created on: 24 Nov 2017
- *      Author: pnikiel
+ *  Created on: 24 Nov, 2017
+ *      Author: Piotr Nikiel <piotr@nikiel.info>
+ *
+ *  This file is part of Quasar.
+ *
+ *  Quasar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public Licence as published by
+ *  the Free Software Foundation, either version 3 of the Licence.
+ *
+ *  Quasar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public Licence for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Quasar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef INCLUDE_UACLIENT_UASESSION_H_

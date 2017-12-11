7f57d669ab8a0d09c3f9baf4f519a63f02113e21
Paris Moschovakos <paris.moschovakos@cern.ch>
2017-12-11 19:08:25 +0100

    Profit from status codes descriptions from open62541-compat

--- src/statuscode.cpp
+++ src/statuscode.cpp
@@ -2,11 +2,13 @@
  * statuscode.cpp
  *
  *  Created on: 21 Jun 2017
- *      Author: pnikiel
+ *      Author: pnikiel, pmoschov
  */
 
 #include <statuscode.h>
 #include <sstream>
+#include <open62541.h>
+#include <string>
 
 template<typename T>
 static std::string toHexString (const T t)
@@ -19,19 +21,10 @@ static std::string toHexString (const T t)
 
 UaString UaStatus::toString() const
 {
-    switch (m_status)
-    {
-        case OpcUa_Good: return "Good";
-        case OpcUa_Bad: return "Bad";
-        case OpcUa_BadParentNodeIdInvalid: return "BadParentNodeIdInvalid";
-        case OpcUa_BadDataEncodingInvalid: return "BadDataEncodingInvalid";
-        case OpcUa_BadServerNotConnected: return "BadServerNotConnected";
-        case OpcUa_BadOutOfRange: return "BadOutOfRange";
-        case OpcUa_BadNotImplemented: return "BadNotImplemented";
-        case OpcUa_BadInternalError: return "BadInternalError";
-        case OpcUa_BadWaitingForInitialData: return "BadWaitingForInitialData";
-        default: return ("statuscode=0x"+toHexString(m_status)+" --missing-description--implement-me-in-statuscode.cpp--").c_str();
-    }
+
+	std::string buf(UA_StatusCode_name(m_status));
+	buf.append(": ");
+	buf.append(UA_StatusCode_explanation(m_status));
+	return buf.c_str();
+
 }
-
-

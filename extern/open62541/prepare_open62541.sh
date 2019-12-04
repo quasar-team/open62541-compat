d8959fd2886f377a967db79879d88b13e186dd89
Piotr Nikiel <piotr@nikiel.info>
2019-12-04 15:50:27 +0100

    some path changes...

--- extern/open62541/prepare_open62541.sh
+++ extern/open62541/prepare_open62541.sh
@@ -8,9 +8,8 @@
 TAG=v0.3.1
 
 # prepare the dir structure
-mkdir open62541
-mkdir open62541/include
-mkdir open62541/src
+mkdir include
+mkdir src
 
 # prepare (i.e. amalgamate... open62541)
 WD=`pwd`
@@ -26,13 +25,13 @@ make || exit
 cd $WD
 
 # deploy files
-find tmp -name open62541.h -ok cp {} open62541/include \; || exit
-find tmp -name open62541.c -ok cp {} open62541/src \; || exit
+find tmp -name open62541.h -ok cp {} include \; || exit
+find tmp -name open62541.c -ok cp {} src \; || exit
 
 read -n 1 -p "Would you like to commit the freshly amalgamated open62541? type y if so." answer
 if [ $answer == "y" ]; then
-  git add open62541/include/open62541.h
-  git add open62541/src/open62541.c
+  git add include/open62541.h
+  git add src/open62541.c
   git commit --author="open62541 <open62541@open62541.org>" -m "Amalgamated open62541 v $TAG by prepare_open62541.sh"
 fi
 

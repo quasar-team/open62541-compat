36c5b78f0fcf589bad30387d3165ab6f35b31d5f
Benjamin Farnham <bfarnham@dev-x64-cc7.cern.ch>
2021-07-16 17:05:37 +0200

    add test (stubbed, certain to fail currently)

---
+++ test/src/uaqualifiedname_test.cpp
@@ -0,0 +1,15 @@
+/*
+ * uaqualifiedname_test.cpp
+ *
+ *  Created on: 16 July 2021
+ *      Author: ben-farnham 
+ */
+
+#include "gtest/gtest.h"
+
+#include <other.h>
+
+TEST(UaQualifiedNameTest, testDefaultConstructor)
+{
+    EXPECT_EQ(true, false) << "WTF ?";
+}

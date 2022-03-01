372f41808570acb7ddeee5c7e086ab0b2503bb88
Paris Moschovakos <paris.moschovakos@cern.ch>
2022-03-01 17:15:08 +0100

    Check everything that has the capacity of taking wrong values

--- src/logit_logger.cpp
+++ src/logit_logger.cpp
@@ -40,8 +40,12 @@ static void logFromOpen62541 (
 		default: logItLogLevel = Log::ERR; // just in case they added a new level.
 	}
 
-	Log::LOG_LEVEL userLogLevel;
-	Log::getComponentLogLevel(Log::getComponentHandle("open62541"), userLogLevel);
+	auto open62541LogItHandle = Log::getComponentHandle("open62541");
+	if (open62541LogItHandle == Log::INVALID_HANDLE) return;
+
+	Log::LOG_LEVEL userLogLevel = Log::INF;
+	if (!Log::getComponentLogLevel(open62541LogItHandle, userLogLevel)) return;
+
 	if (userLogLevel > logItLogLevel) return;
 
 	// translate open62541 category.

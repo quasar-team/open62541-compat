9182e4d43734c5ee745d7d632c2edb20938c9d1f
Paris <paris.moschovakos@cern.ch>
2026-06-10 22:47:15 +0200

    uabasenodes.h: UA-SDK Ua_AccessLevel_* and UaVariable_Value_* constants (values per UA-SDK 1.6.5 uabase.h/uabasenodes.h)

--- include/uabasenodes.h
+++ include/uabasenodes.h
@@ -27,6 +27,17 @@
 #include <uadatavalue.h>
 #include <session.h>
 
+const OpcUa_UInt32 Ua_AccessLevel_None         = 0x0;
+const OpcUa_UInt32 Ua_AccessLevel_CurrentRead  = 0x1;
+const OpcUa_UInt32 Ua_AccessLevel_CurrentWrite = 0x2;
+const OpcUa_UInt32 Ua_AccessLevel_HistoryRead  = 0x4;
+const OpcUa_UInt32 Ua_AccessLevel_HistoryWrite = 0x8;
+
+const OpcUa_UInt32 UaVariable_Value_None                    = 0x0;
+const OpcUa_UInt32 UaVariable_Value_Cache                   = 0x1;
+const OpcUa_UInt32 UaVariable_Value_CacheIsSource           = 0x2;
+const OpcUa_UInt32 UaVariable_Value_CacheIsUpdatedOnRequest = 0x4;
+
 class UaVariable: public UaNode
 {
 public:
